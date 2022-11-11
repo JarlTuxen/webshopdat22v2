2ab29db07ed5523547fc4e0921bfbaf7c54fdeed
jarltuxen <jarl.tuxen@gmail.com>
2022-11-11 13:24:45 +0100

    Singleton ConnectionManager

--- src/main/java/com/example/webshopdat22v2/repository/ProductRepository.java
+++ src/main/java/com/example/webshopdat22v2/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.example.webshopdat22v2.repository;
 
 import com.example.webshopdat22v2.model.Product;
 
+import com.example.webshopdat22v2.util.ConnectionManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
@@ -26,9 +27,12 @@ public class ProductRepository {
         List<Product> productList = new LinkedList<>();
 
         try {
-            Connection conn = DriverManager.getConnection(db_url, uid, pwd);
+            //Connection conn = DriverManager.getConnection(db_url, uid, pwd);
                     //"jdbc:mysql://localhost:3306/webshop",
                     //"webshop_dat22v2", "DetErSnartJul!");
+            //ConnectionManager connMgr = new ConnectionManager();
+            //Connection conn = connMgr.getConnection();
+            Connection conn = ConnectionManager.getConnection(db_url, uid, pwd);
             PreparedStatement psts = conn.prepareStatement("SELECT * from product");
             ResultSet resultSet = psts.executeQuery();
 
@@ -53,9 +57,10 @@ public class ProductRepository {
 
     public void addProduct(Product newProduct) {
         try {
-            Connection conn = DriverManager.getConnection(
+            Connection conn = ConnectionManager.getConnection(db_url, uid, pwd);
+            /*Connection conn = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/webshop",
-                    "root", "qJiw03K2zwJD");
+                    "root", "qJiw03K2zwJD");*/
             String queryCreate = "INSERT INTO product (name, price) VALUES (?, ?)";
             PreparedStatement psts = conn.prepareStatement(queryCreate);
 
@@ -79,9 +84,10 @@ public class ProductRepository {
         product.setId(id);
 
         try {
-            Connection conn = DriverManager.getConnection(
+            Connection conn = ConnectionManager.getConnection(db_url, uid, pwd);
+            /* Connection conn = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/webshop",
-                    "root", "qJiw03K2zwJD");
+                    "root", "qJiw03K2zwJD");*/
             String queryCreate = "SELECT * FROM product WHERE id=?";
             PreparedStatement psts = conn.prepareStatement(queryCreate);
 
@@ -108,9 +114,11 @@ public class ProductRepository {
 
     public void updateProduct(Product product){
         try {
+            Connection conn = ConnectionManager.getConnection(db_url, uid, pwd);
+            /*
             Connection conn = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/webshop",
-                    "root", "qJiw03K2zwJD");
+                    "root", "qJiw03K2zwJD");*/
             String queryCreate = "UPDATE product " +
                     "SET name=?, price=? WHERE id=?";
             PreparedStatement psts = conn.prepareStatement(queryCreate);
@@ -132,9 +140,10 @@ public class ProductRepository {
 
     public void deleteProductById(int sletId){
         try {
-            Connection conn = DriverManager.getConnection(
+            Connection conn = ConnectionManager.getConnection(db_url, uid, pwd);
+            /*Connection conn = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/webshop",
-                    "root", "qJiw03K2zwJD");
+                    "root", "qJiw03K2zwJD");*/
             String queryCreate = "DELETE FROM product WHERE id=?";
             PreparedStatement psts = conn.prepareStatement(queryCreate);
 
---
+++ src/main/java/com/example/webshopdat22v2/util/ConnectionManager.java
@@ -0,0 +1,27 @@
+package com.example.webshopdat22v2.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+    private static Connection conn;
+
+    public static Connection getConnection(String db_url, String uid, String pwd) {
+
+        if (conn != null) return conn;
+
+        /*String db_url = System.getenv("JDBCUrl");
+        String uid = System.getenv("JDBCUsername");
+        String pwd = System.getenv("JDBCPassword");*/
+        try {
+            conn = DriverManager.getConnection(db_url, uid, pwd);
+
+        } catch (SQLException e) {
+            System.out.println("Couldn't connect to db");
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
